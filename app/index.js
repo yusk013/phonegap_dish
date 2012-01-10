24ce30d755b539d6c39a996aa85452ea0f180a7f
yusk013 <yusk013@126.com>
2012-01-10 17:00:42 +0800

    1.paged 2.category paged

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
-﻿var cateTmpl = "<ul>{{#category}}<li><a href='#c{{id}}'>{{name}}</a></li>{{/category}}</ul>";
+﻿var pageIndex = 1;
+var pageSize = 3;
+
+var cateTmpl = "<ul>{{#category}}<li><a href='#c{{id}}' cid='{{id}}'>{{name}}</a></li>{{/category}}</ul>";
 var dishTmpl = "{{#dishes}}<article style='background-image:url(img/{{img}})' data-id='{{id}}'><header><h2>{{name}}</h2></header><section class='des'>{{des}}</section><section class='price' data-p='{{price}}' data-vp='{{vipPrice}}'><ul><li>{{price}}</li><li>{{vipPrice}}</li></ul></section></article>{{/dishes}}";
 var $ = function (str) {
     return document.querySelector(str);
@@ -6,20 +9,66 @@ var $ = function (str) {
 var $a = function (str) {
     return document.querySelectorAll(str);
 }
+Array.prototype.contains = function (key) {
+    for (var i in this) {
+        if (this[i] == key) {
+            return true;
+        }
+    }
+    return false;
+};
+
 
 document.addEventListener("deviceready", onDeviceReady, false);
+
+var bindEvent = function () {
+    $("footer a:nth-child(1)").addEventListener("click", function () {
+        if (pageIndex > 1) pageIndex--;
+        bindDishes();
+    });
+    $("footer a:nth-child(2)").addEventListener("click", function () {
+        //        if (pageIndex > 1) pageIndex--;
+        //        bindDishes();
+    });
+    $("footer a:nth-child(3)").addEventListener("click", function () {
+        if (menus.pages.length > pageIndex) pageIndex++;
+        bindDishes();
+    });
+    var categoryTabs = $a("nav li a");
+    for (var i in categoryTabs) {
+        categoryTabs[i].addEventListener("click", bindCategoryDishes);
+    }
+};
 
 var onDeviceReady = function () {
     $("header h1").innerText = menus.corp.name;
     $("nav").innerHTML = Mustache.to_html(cateTmpl, menus);
-    bindDishes(1);
+    bindDishes();
+    bindEvent();
+};
+var bindCategoryDishes = function () {
+    var cid = parseInt(this.getAttribute("cid"));
+    for (var i in menus.pages) {
+        if (menus.pages[i].cateId == cid) {
+            pageIndex = parseInt(i) + 1;
+            break;
+        }
+    }
+    bindDishes();
 };
 
-var bindDishes = function (pages) {
-    if (!pages) pages = 1;
-    var all = menus.dishes.length;
-    var start = (pages - 1) * 3;
-    var end = start + 3;
-    var currentDishes = menus.dishes.slice(start, end);
+var bindDishes = function () {
+    var page = menus.pages[pageIndex - 1];
+    if (!page) return;
+    var currentDishes = [];
+    for (var j in page.dishes) {
+        for (var i in menus.dishes) {
+            if (page.dishes[j] == menus.dishes[i].id) {
+                currentDishes.push(menus.dishes[i]);
+                break;
+            }
+        }
+    }
+
     $("section").innerHTML = Mustache.to_html(dishTmpl, { dishes: currentDishes });
 };
--- app/json.js
+++ app/json.js
@@ -1,141 +1,57 @@
 ﻿var menus = {
-corp:{
-name: '海底捞火锅'
-}, 
-category:[{ id: 1, name: '凉菜' }, { id: 2, name: '热菜' }, { id: 3, name: '主食' }, { id: 4, name: '酒水'}, {id: 5, name: '苗疆菜'}], 
-dishes:[{
-    id: 1,
-    name: '鱼香肉丝',
-    category: 2,
-    level: 1,
-    des: '精品川菜',
-    price: 25,
-    vipPrice: 23,
-    img: 'yxrs.jpg'
-}, {
-    id: 2,
-    name: '宫保鸡丁',
-    category: 2,
-    level: 2,
-    des: '精品川菜',
-    price: 25,
-    vipPrice: 23,
-    img: 'gbjd.jpg'
-}, {
-    id: 3,
-    name: '百合虾仁',
-    category: 2,
-    level: 1,
-    des: '口感香甜',
-    price: 50,
-    vipPrice: 48,
-    img: 'bhxr.jpg'
-}, {
-    id: 4,
-    name: '四喜丸子',
-    category: 2,
-    level: 3,
-    des: '精品丸子',
-    price: 36,
-    vipPrice: 32,
-    img: 'sxwz.jpg'
-}, {
-    id: 5,
-    name: '卤水拼盘',
-    category: 1,
-    level: 1,
-    des: '精品凉菜',
-    price: 35,
-    vipPrice: 32,
-    img: 'lspp.jpg'
-}, {
-    id: 6,
-    name: '千层饼',
-    category: 3,
-    level: 1,
-    des: '主食',
-    price: 18,
-    vipPrice: 16,
-    img: 'qcb.jpg'
-}, {
-    id: 7,
-    name: '五粮液国宴酒',
-    category: 4,
-    level: 1,
-    des: '正品五粮液',
-    price: 525,
-    vipPrice: 515,
-    img: 'wlygy.jpg'
-}]
+    corp: {
+        name: '海底捞火锅'
+    },
+    category: [{ id: 1, name: '凉菜' }, { id: 2, name: '热菜' }, { id: 3, name: '主食' }, { id: 4, name: '酒水'}],
+    dishes: [{
+        id: 1,
+        name: '鱼香肉丝',
+        des: '精品川菜',
+        price: 25,
+        vipPrice: 23,
+        img: 'yxrs.jpg'
+    }, {
+        id: 2,
+        name: '宫保鸡丁',
+        des: '精品川菜',
+        price: 25,
+        vipPrice: 23,
+        img: 'gbjd.jpg'
+    }, {
+        id: 3,
+        name: '百合虾仁',
+        des: '口感香甜',
+        price: 50,
+        vipPrice: 48,
+        img: 'bhxr.jpg'
+    }, {
+        id: 4,
+        name: '四喜丸子',
+        des: '精品丸子',
+        price: 36,
+        vipPrice: 32,
+        img: 'sxwz.jpg'
+    }, {
+        id: 5,
+        name: '卤水拼盘',
+        des: '精品凉菜',
+        price: 35,
+        vipPrice: 32,
+        img: 'lspp.jpg'
+    }, {
+        id: 6,
+        name: '千层饼',
+        des: '主食',
+        price: 18,
+        vipPrice: 16,
+        img: 'qcb.jpg'
+    }, {
+        id: 7,
+        name: '五粮液国宴酒',
+        des: '正品五粮液',
+        price: 525,
+        vipPrice: 515,
+        img: 'wlygy.jpg'
+    }],
+    pages: [{ layout: 1, dishes: [5, 3, 1], cateId: 1 }, { layout: 1, dishes: [1, 2, 3], cateId: 2 }, { layout: 1, dishes: [6, 4, 1], cateId: 3 }, { layout: 1, dishes: [7, 3, 2], cateId: 4}]
 };
-/*
-var profile = {
-    corpName: '海底捞火锅'
-};
-var foodLevel = [{ id: 1, name: '精品菜品' }, { id: 2, name: '今日特价' }, { id: 3, name: '特色菜'}];
-var foodCategory = 
-var foodMenu = [{
-    id: 1,
-    name: '鱼香肉丝',
-    category: 2,
-    level: 1,
-    des: '精品川菜',
-    price: 25,
-    vipPrice: 23,
-    img: 'yxrs.jpg'
-}, {
-    id: 2,
-    name: '宫保鸡丁',
-    category: 2,
-    level: 2,
-    des: '精品川菜',
-    price: 25,
-    vipPrice: 23,
-    img: 'gbjd.jpg'
-}, {
-    id: 3,
-    name: '百合虾仁',
-    category: 2,
-    level: 1,
-    des: '口感香甜',
-    price: 50,
-    vipPrice: 48,
-    img: 'bhxr.jpg'
-}, {
-    id: 4,
-    name: '四喜丸子',
-    category: 2,
-    level: 3,
-    des: '精品丸子',
-    price: 36,
-    vipPrice: 32,
-    img: 'sxwz.jpg'
-}, {
-    id: 5,
-    name: '卤水拼盘',
-    category: 1,
-    level: 1,
-    des: '精品凉菜',
-    price: 35,
-    vipPrice: 32,
-    img: 'lspp.jpg'
-}, {
-    id: 6,
-    name: '千层饼',
-    category: 3,
-    level: 1,
-    des: '主食',
-    price: 18,
-    vipPrice: 16,
-    img: 'qcb.jpg'
-}, {
-    id: 7,
-    name: '五粮液国宴酒',
-    category: 4,
-    level: 1,
-    des: '正品五粮液',
-    price: 525,
-    vipPrice: 515,
-    img: 'wlygy.jpg'
-}];
-*/
